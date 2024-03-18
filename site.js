e254c916712aa6e13c7e1f02dc8fe5bdbc53af1c
BenitoSupreme <benschr8r@gmail.com>
2024-03-17 19:41:39 -0500

    page hopping

--- site.js
+++ site.js
@@ -1,26 +1,97 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Get navbar burger and menu
+  const navbarBurger = document.querySelector(".navbar-burger");
+  const navbarMenu = document.querySelector(".navbar-menu");
+
+  // Toggle navbar menu on burger click
+  navbarBurger.addEventListener("click", () => {
+    navbarBurger.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
+  });
   // Find the link elements by their IDs
   var homePageLink = document.getElementById("homePageLink");
   var executiveLeadersLink = document.getElementById("executiveLeadersLink");
+  var votingLink = document.getElementById("votingLink");
+  var pastEventsLink = document.getElementById("pastEventsLink");
+  var aboutUsLink = document.getElementById("aboutUsLink");
+  var testLink = document.getElementById("testLink");
 
   // Find the section elements by their IDs
   var homePage = document.getElementById("homePage");
   var executiveLeadersPage = document.getElementById("executiveLeadersPage");
+  var votingPage = document.getElementById("votingPage");
+  var pastEventsPage = document.getElementById("pastEventsPage");
+  var aboutUsPage = document.getElementById("aboutUsPage");
+  var testPage = document.getElementById("testPage");
 
-  // Add event listeners to the links
+  // Click home page nav actions
   homePageLink.addEventListener("click", function (event) {
     homePage.style.display = "block";
 
     // hide other html pages
     executiveLeadersPage.style.display = "none";
+    votingPage.style.display = "none";
+    pastEventsPage.style.display = "none";
+    aboutUsPage.style.display = "none";
   });
 
+  // click executive leaders page nav actions
   executiveLeadersLink.addEventListener("click", function (event) {
     // Prevent the default link behavior
     executiveLeadersPage.style.display = "block";
 
     // hide other html page
     homePage.style.display = "none";
+    votingPage.style.display = "none";
+    pastEventsPage.style.display = "none";
+    aboutUsPage.style.display = "none";
+  });
+
+  // click voting page page nav actions
+  votingLink.addEventListener("click", function (event) {
+    // Prevent the default link behavior
+    votingPage.style.display = "block";
+
+    // hide other html page
+    homePage.style.display = "none";
+    executiveLeadersPage.style.display = "none";
+    pastEventsPage.style.display = "none";
+    aboutUsPage.style.display = "none";
+  });
+
+  // click voting page page nav actions
+  pastEventsLink.addEventListener("click", function (event) {
+    // Prevent the default link behavior
+    pastEventsPage.style.display = "block";
+
+    // hide other html page
+    homePage.style.display = "none";
+    executiveLeadersPage.style.display = "none";
+    votingPage.style.display = "none";
+    aboutUsPage.style.display = "none";
+  });
+
+  // click about us page nav actions
+  aboutUsLink.addEventListener("click", function (event) {
+    // Prevent the default link behavior
+    aboutUsPage.style.display = "block";
+
+    // hide other html page
+    homePage.style.display = "none";
+    executiveLeadersPage.style.display = "none";
+    votingPage.style.display = "none";
+    executiveLeadersPage.style.display = "none";
+  });
+
+  // click about us page nav actions
+  testLink.addEventListener("click", function (event) {
+    // Prevent the default link behavior
+    testPage.style.display = "block";
+
+    // hide other html page
+    homePage.style.display = "none";
+    executiveLeadersPage.style.display = "none";
+    votingPage.style.display = "none";
   });
 
   // FullCalendar initialization
